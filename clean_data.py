f13137ea716e5134915cffb4013118d4f3997ffc
Ankit Vishwakarma <61557454+ankitv1004@users.noreply.github.com>
2024-09-02 22:30:59 +0530

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,6 @@
 def clean_data(**kwargs):
     import pandas as pd
+    import logging
     ti = kwargs['ti']
     df_customers=kwargs['ti'].xcom_pull(key='df_customers', task_ids='data_extract')
     df_orders=kwargs['ti'].xcom_pull(key='df_orders', task_ids='data_extract')
@@ -16,9 +17,12 @@ def clean_data(**kwargs):
     df_reviews = pd.DataFrame(df_reviews)
 
     
-    
-    # print(df_customers,df_orders,df_order_items,df_products,df_categories,df_reviews)
-    # print(df_customers.dtypes,df_orders.dtypes,df_order_items.dtypes,df_products.dtypes,df_categories.dtypes,df_reviews.dtypes)
+    df_customers = df_customers.drop_duplicates(subset=['customer_id'], keep=False)
+    df_customers = df_customers.dropna(subset=['customer_id'], how='all')
+    df_orders = df_orders.dropna(subset=['order_id'], how='all')
+    df_products = df_products.dropna(subset=['product_id'], how='all')
+    df_reviews = df_reviews.dropna(subset=['review_id'], how='all')
+    df_categories = df_categories.dropna(subset=['category_id'], how='all')
     
     df_customers = df_customers.to_dict()
     df_orders = df_orders.to_dict()
@@ -34,3 +38,14 @@ def clean_data(**kwargs):
     kwargs['ti'].xcom_push(key='df_products', value=df_products)
     kwargs['ti'].xcom_push(key='df_categories', value=df_categories)
     kwargs['ti'].xcom_push(key='df_reviews', value=df_reviews)
+    
+    logger = logging.getLogger("airflow.task")
+    task_instance = kwargs['ti']
+    task_id = task_instance.task_id
+    dag_id = task_instance.dag_id
+    run_id = task_instance.run_id
+
+    # Log task information
+    logger.info(f"Running task _Test: {task_id}")
+    logger.info(f"DAG ID: {dag_id}")
+    logger.info(f"Run ID: {run_id}")

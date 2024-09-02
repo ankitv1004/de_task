c30dc893545acf3d6222839ce497772d2df63df2
Ankit Vishwakarma <61557454+ankitv1004@users.noreply.github.com>
2024-09-02 22:32:43 +0530

    Update etl_dag.py

--- etl_dag.py
+++ etl_dag.py
@@ -1,11 +1,14 @@
-from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import logging
 import pandas as pd
 from data_extract import data_extract
 from clean_data import clean_data
 from transform_data import transform_data
 from insert_data import insert_data
+
+
 
 # Define default arguments for the DAG
 default_args = {
@@ -16,13 +19,15 @@ default_args = {
 dag = DAG(
     'etl_dag',
     default_args=default_args,
-    schedule_interval=None,  # Set the schedule as needed
+    schedule_interval=timedelta(hours=3),  # Set the schedule as needed
     catchup=False,
 )
 
 data_extract_task = PythonOperator(
     task_id='data_extract',
     python_callable=data_extract,
+    retries=1,
+    retry_delay=timedelta(seconds=20),
     provide_context=True,
     dag=dag,
 )
@@ -30,6 +35,8 @@ data_extract_task = PythonOperator(
 clean_data_task = PythonOperator(
     task_id='clean_data',
     python_callable=clean_data,
+    retries=1,
+    retry_delay=timedelta(seconds=20),
     provide_context=True,
     dag=dag,
 )
@@ -37,6 +44,8 @@ clean_data_task = PythonOperator(
 transform_data_task = PythonOperator(
     task_id='transform_data',
     python_callable=transform_data,
+    retries=1,
+    retry_delay=timedelta(seconds=20),
     provide_context=True,
     dag=dag,
 )

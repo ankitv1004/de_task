9d9b44306e4d9897154ba0fe87d1b4eed166e932
Ankit Vishwakarma <61557454+ankitv1004@users.noreply.github.com>
2024-09-03 10:31:35 +0530

    Update etl_dag.py

--- etl_dag.py
+++ etl_dag.py
@@ -16,13 +16,14 @@ default_args = {
     'start_date': datetime(2024, 9, 1),
     'retries': 0,
 }
+#DAG
 dag = DAG(
     'etl_dag',
     default_args=default_args,
     schedule_interval=timedelta(hours=3),  # Set the schedule as needed
     catchup=False,
 )
-
+#Task for data extraction
 data_extract_task = PythonOperator(
     task_id='data_extract',
     python_callable=data_extract,
@@ -32,6 +33,7 @@ data_extract_task = PythonOperator(
     dag=dag,
 )
 
+#Task for Data cleaning
 clean_data_task = PythonOperator(
     task_id='clean_data',
     python_callable=clean_data,
@@ -41,6 +43,7 @@ clean_data_task = PythonOperator(
     dag=dag,
 )
 
+#Task for Transforming data
 transform_data_task = PythonOperator(
     task_id='transform_data',
     python_callable=transform_data,
@@ -50,6 +53,7 @@ transform_data_task = PythonOperator(
     dag=dag,
 )
 
+#Task for inserting data in Mongodb
 insert_data_task = PythonOperator(
     task_id='insert_data',
     python_callable=insert_data,
